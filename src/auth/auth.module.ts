86dd9cbcfb4455a3f83a37764a2d91a4120b08a5
Jorge Nicolás Fernández Gamarra <nicolas.fernandezgamarra@gmail.com>
2026-01-27 15:36:52 -0300

    colocar guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controllar';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+            secret: configService.get<string>('JWT_SECRET'),
+            signOptions: { expiresIn: '24h' },
+        }),
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
 export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { UsersService } from "../users/user.service";
+import { JwtService } from "@nestjs/jwt";
+import * as bcrypt from "bcrypt";
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async signIn(email: string, pass: string) {
+    const user = await this.usersService.findOneByEmail(email);
+
+    if (!user || !(await bcrypt.compare(pass, user.password))) {
+        throw new UnauthorizedException('Credenciales incorrectas');
+    }
+
+    const payload = { sub: user.id, email: user.email, role: user.role };
+    
+    return {
+        access_token: await this.jwtService.signAsync(payload),
+        user: {
+            email: user.email,
+            role: user.role,
+        }
+    };
+  }
+}
